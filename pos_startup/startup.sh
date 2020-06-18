e404f5663034f5e76d01bcb6d082402f15242321
Grégory Schreiner <gregory@mokatourisme.fr>
2020-06-18 16:12:49 +0000

    Update startup.sh

--- pos_startup/startup.sh
+++ pos_startup/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "Démarrage du point de vente"
+echo "Démarrage du point de vente..."
 
 # Ecran 1
 echo "Ecran principal..."
-chromium-browser --window-position=0,0 --noerrdialogs --kiosk --disable-translate --disable-features=TranslateUI ${URL}
+--kiosk --new-window --window-position=0,0 --noerrdialogs --disable-translate --disable-features=TranslateUI ${URL} &
 
 # Ecran 2
 echo "Afficheur client..."
-chromium-browser --window-position=1400,0 --noerrdialogs --kiosk --disable-translate --disable-features=TranslateUI http://localhost:8069/point_of_sale/display
+google-chrome --kiosk --new-window --window-position=1367,0 --noerrdialogs --disable-translate --disable-features=TranslateUI http://localhost:8069/point_of_sale/display &
